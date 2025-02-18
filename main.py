9b395752c78018e826b9841faddd92958be81855
KimNattanan <jeung.nattanan@gmail.com>
2025-02-18 16:56:20 +0700

    done

--- main.py
+++ main.py
@@ -3,10 +3,10 @@ import trainer
 trainer.train_bot(save_name='duel_vbot', plot_name='duel_vbot', display=True)
 
 trainer.train_ai(save_name='duel_vbot_20000_vai_l', save_name2='duel_vbot_20000_vai_r',
-                 checkpoint_path='./checkpoints/duel_vbot_20000_vai_l_37400.pt',
-                 checkpoint_path2='./checkpoints/duel_vbot_20000_vai_r_37400.pt',
+                 checkpoint_path='./checkpoints/duel_vbot_20000.pt',
+                 checkpoint_path2='./checkpoints/duel_vbot_20000.pt',
                  plot_name='duel_vbot_20000_vai',
-                 number_of_ep=40005, begin_ep=37401, start_epsilon=0.1,
+                 number_of_ep=40005, begin_ep=20001, start_epsilon=0.4,
                  display=True)
 trainer.train_ai_double(save_name='duel_vbot_20000_vai_double_l', save_name2='duel_vbot_20000_vai_double_r',
                         checkpoint_path='./checkpoints/duel_vbot_20000.pt',
